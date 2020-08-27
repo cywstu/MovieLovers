f93980b70b062bee8eb6084e80fe36f381e0827d
cywstu <46858924+cywstu@users.noreply.github.com>
2020-08-28 00:51:20 +0800

    long time

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+    res.send("Hello World");
 });
 
-app.listen(port, () => console.log('listening on port ' + port));
+app.get("/movie", (req, res) =>{
+    res.send("movie page");
+});
+
+app.get("/mypage", (req, res) =>{
+    res.send("user");
+});
+
+app.get("/search", (req, res) =>{
+    res.send("search");
+});
+
+app.listen(port, () => console.log("listening on port " + port));
